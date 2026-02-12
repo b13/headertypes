628ea4e7db4fa79bc386648790e630bac036598b
Johannes Schlier <johannes.schlier@b13.com>
2026-02-12 09:22:40 +0100

    [TASK] Release 1.2.0
    TYPO3 14 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,5 +8,5 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH, Stuttgart',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.2.0',
 ];
